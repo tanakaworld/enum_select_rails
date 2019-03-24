14de4fd640b2abfeb410d8fa07f5a09b6de6a830
tanakaworld <yutaro.tanaka.world@gmail.com>
2019-03-24 21:45:11 +0900

    chore(example): run db:migrate

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180328024353) do
+ActiveRecord::Schema.define(version: 2018_03_28_024353) do
 
   create_table "users", force: :cascade do |t|
     t.integer "gender"
